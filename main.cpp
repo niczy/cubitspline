21db6dfb3bffe16a3921326496d8bf56734957dd
Nicholas <nicholas.zhaoyu@gmail.com>
2015-03-08 07:43:06 -0700

    Remove comments.

--- main.cpp
+++ main.cpp
@@ -9,24 +9,6 @@
 using namespace std;
 // member function
 
-/*
-CYield Curve
-
-Private
-int last
-int first
-int count
-maxTerm
-vector <int> term
-char  curr
-
-Public
-Double GetDiscountFactor()
-Double GetOneMonthVolatility()
-
-CYieldCurve(curr, maxTerm)
-*/
-
 class CYieldCurve {
   private:
     tk::spline s;
@@ -59,14 +41,6 @@ class CYieldCurve {
     }
 };
 
-
-/* Yield Curve
-This block will read daily historical yield curves. It will smooth each of them it using cubic splines. For each
-term (and each historical date), two types of output will be created: smoothed yield and smoothed discount factor. It will also estimate the historical (absolute) volatility of the smoothed one-month yield, and the speed of mean-reversion alpha of the one-month yield. In a first time, we will assume that the one-month yield
-follows non standard Brownian motion. You will then hard code alpha = 0.2. use cubic spline interpolation (source code I have found online).
- 
- methodology online:
-http://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/yieldmethod.aspx */
 int main(int argc, char** argv) {
    
   int TOTAL_TIME = 11;
